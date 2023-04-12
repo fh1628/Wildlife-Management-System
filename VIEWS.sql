326d0e209fc5735b69045974847e7276069ea07f
yousscharif <youssecharif15@gmail.com>
2023-04-12 14:11:16 +0300

    Finish everything

--- VIEWS.sql
+++ VIEWS.sql
@@ -1,7 +1,7 @@
--- CREATE VIEW HABITAT_LOCATION_DETAILS AS
--- SELECT Habitat.HabitatName, Location.LocationName
--- FROM Habitat
--- JOIN Location ON Habitat.Longitude = Location.Longitude AND Habitat.Latitude = Location.Latitude;
+CREATE VIEW HABITAT_LOCATION_DETAILS AS
+SELECT Habitat.HabitatName, Location.LocationName
+FROM Habitat
+JOIN Location ON Habitat.Longitude = Location.Longitude AND Habitat.Latitude = Location.Latitude;
 
 CREATE VIEW POPULATION_HABITAT_DETAILS AS
 SELECT Population.SpeciesScientificName, Habitat.*
