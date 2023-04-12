3785d93fcd4dcf108c809e5c0852cc0d96bf0bb1
fh1628 <faridahaddad13@gmail.com>
2023-04-12 13:42:05 +0300

    Query 4

--- QUERIES.sql
+++ QUERIES.sql
@@ -21,37 +21,49 @@
 
 -- Query 2: List all habitats occupied by a given species name (input is the Species Common Name)
 
-DELIMITER //
+-- DELIMITER //
 
-CREATE PROCEDURE get_habitats_of_species(IN species_common_name VARCHAR(255))
-BEGIN
-SELECT PopulationHabitat.HabitatName as 'Habitat Name', 
-	   PopulationHabitat.HabitatType as 'Habitat Type',
-	   PopulationHabitat.DegradationLevel as 'Degradation Level', 
-       PopulationHabitat.ConservationStatus as 'Conservation Status'
-FROM POPULATION_HABITAT_DETAILS PopulationHabitat
-JOIN POPULATION_SPECIES_DETAILS PopulationSpecies ON PopulationHabitat.SpeciesScientificName = PopulationSpecies.SpeciesScientificName
-WHERE PopulationSpecies.CommonName=species_common_name;
-END //
+-- CREATE PROCEDURE get_habitats_of_species(IN species_common_name VARCHAR(255))
+-- BEGIN
+-- SELECT PopulationHabitat.HabitatName as 'Habitat Name', 
+-- 	   PopulationHabitat.HabitatType as 'Habitat Type',
+-- 	   PopulationHabitat.DegradationLevel as 'Degradation Level', 
+--     PopulationHabitat.ConservationStatus as 'Conservation Status'
+-- FROM POPULATION_HABITAT_DETAILS PopulationHabitat
+-- JOIN POPULATION_SPECIES_DETAILS PopulationSpecies ON PopulationHabitat.SpeciesScientificName = PopulationSpecies.SpeciesScientificName
+-- WHERE PopulationSpecies.CommonName=species_common_name;
+-- END //
 
-DELIMITER ;
+-- DELIMITER ;
 
-CALL get_habitats_of_species('Giant Panda');
+-- CALL get_habitats_of_species('Giant Panda');
 
 
 
 
 -- Query 3:
-DELIMITER //
-CREATE PROCEDURE get_population_of_species(IN species_name VARCHAR(255))
-BEGIN
-	SELECT Population.PopulationID as 'Population ID', Population.Size as 'Size'
-    FROM Population
-    WHERE Population.SpeciesScientificName = species_name;
-END //
-DELIMITER ;
+-- DELIMITER //
+-- CREATE PROCEDURE get_population_of_species(IN species_name VARCHAR(255))
+-- BEGIN
+-- SELECT Population.PopulationID as 'Population ID', Population.Size as 'Size'
+--    FROM Population
+--   WHERE Population.SpeciesScientificName = species_name;
+-- END //
+-- DELIMITER ;
 
-CALL get_population_of_species('Panthera tigris');
+-- CALL get_population_of_species('Panthera tigris');
 
+
+-- Query 4: List all researchers studying a specific species or population
+-- DELIMITER //
+-- CREATE PROCEDURE get_researchers_of_species(IN species_name VARCHAR(255))
+-- BEGIN
+-- 	SELECT Researcher.Email as 'Email', Researcher.Name as 'Name', Researcher.Phone as 'Phone Number'
+--   FROM Researcher
+--   WHERE Researcher.SpeciesScientificName = species_name;
+-- END //
+-- DELIMITER ;
+
+-- CALL get_researchers_of_species('Panthera tigris');
 
 
