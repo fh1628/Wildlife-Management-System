40d0e141e3cf9450d94b5f223fd42878a89b42d3
Elena Ghazi <elenaghazi@Elenas-MacBook-Air.local>
2023-04-17 12:13:07 +0300

    Adding the classes for all the entities in Flask.

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost
 # Create a SQLAlchemy object to interact with the database
 db = SQLAlchemy(app)
 
-
 # Define the Location model class
 class Location(db.Model):
     __tablename__ = 'Location'
@@ -33,12 +32,86 @@ class Habitat(db.Model):
     Longitude = db.Column(db.Float, db.ForeignKey('Location.Longitude'), nullable=True)
     Location = db.relationship(Location, backref='habitats')
 
-# Define the HThreats model class
+# Define the Habitat Threats model class
 class HThreats(db.Model):
     __tablename__ = 'HThreats'
     HabitatName = db.Column(db.String(255), db.ForeignKey('Habitat.HabitatName'), primary_key=True)
     Threat = db.Column(db.String(255), primary_key=True)
 
+
+# Define the Species model class
+class Species(db.Model):
+    __tablename__ = 'Species'
+    ScientificName = db.Column(db.String(255), primary_key=True)
+    CommonName = db.Column(db.String(255))
+    ConservationStatus = db.Column(db.String(50))
+    GeographicDistribution = db.Column(db.String(255))
+
+
+# Define the Species Threats model class
+class SThreats(db.Model):
+    __tablename__ = 'SThreats'
+    ScientificName = db.Column(db.String(255), db.ForeignKey('Species.ScientificName', ondelete='CASCADE'), primary_key=True)
+    Threat = db.Column(db.String(255), primary_key=True)
+
+
+# Define the Population model class
+class Population(db.Model):
+    __tablename__ = 'Population'
+    PopulationID = db.Column(db.Integer, primary_key=True)
+    Size = db.Column(db.Integer)
+    Trend = db.Column(db.String(50))
+    GrowthRate = db.Column(db.DECIMAL(5, 2))
+    Density = db.Column(db.DECIMAL(10, 2))
+    HabitatName = db.Column(db.String(255), db.ForeignKey('Habitat.HabitatName', ondelete='CASCADE'))
+    SpeciesScientificName = db.Column(db.String(255), db.ForeignKey('Species.ScientificName', ondelete='CASCADE'))
+
+
+# Define the Researcher model class
+class Researcher(db.Model):
+    __tablename__ = 'Researcher'
+    Name = db.Column(db.String(255))
+    Email = db.Column(db.String(255), primary_key=True)
+    Phone = db.Column(db.String(255))
+    Expertise = db.Column(db.String(255))
+    SpeciesScientificName = db.Column(db.String(255))
+    PopulationID = db.Column(db.Integer, db.ForeignKey('Population.PopulationID', ondelete='SET NULL'))
+
+# Define the Assistant Researcher model class. It's a weak entity of Researcher
+class AssistantResearcher(db.Model):
+    __tablename__ = 'AssistantResearcher'
+    Name = db.Column(db.String(255))
+    Email = db.Column(db.String(255), primary_key=True)
+    ResearcherEmail = db.Column(db.String(255), db.ForeignKey('Researcher.Email', ondelete='CASCADE'))
+    researcher = db.relationship('Researcher', backref=db.backref('assistant_researchers', lazy=True))
+
+# Define the Research Projects model class
+class RProjects(db.Model):
+    __tablename__ = 'RProjects'
+    Email = db.Column(db.String(255), db.ForeignKey('Researcher.Email', ondelete='CASCADE'), primary_key=True)
+    Project = db.Column(db.String(255))
+
+# Define the Research Interests model class
+class RInterests(db.Model):
+    __tablename__ = 'RInterests'
+    Email = db.Column(db.String(255), db.ForeignKey('Researcher.Email', ondelete='CASCADE'), primary_key=True)
+    ResearchInterests = db.Column(db.String(50))
+
+# Define the University Researcher model class
+class UniversityResearcher(db.Model):
+    __tablename__ = 'UniversityResearcher'
+    Name = db.Column(db.String(255))
+    UniversityName = db.Column(db.String(255))
+    Tenure = db.Column(db.String(255))
+    Email = db.Column(db.String(255), db.ForeignKey('Researcher.Email', ondelete='CASCADE'), primary_key=True)
+
+# Define the Company Researcher model class
+class CompanyResearcher(db.Model):
+    __tablename__ = 'CompanyResearcher'
+    Name = db.Column(db.String(255))
+    CompanyName = db.Column(db.String(255))
+    JobTitle = db.Column(db.String(255))
+    Email = db.Column(db.String(255), db.ForeignKey('Researcher.Email', ondelete='CASCADE'), primary_key=True)
 
 with app.app_context():
     # Create the database tables (if they don't exist)
