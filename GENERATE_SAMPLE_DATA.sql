324c69b6c561e253df682f5d690aa6c7e1f04055
fh1628 <faridahaddad13@gmail.com>
2023-04-20 00:41:22 +0300

    filled table

--- GENERATE_SAMPLE_DATA.sql
+++ GENERATE_SAMPLE_DATA.sql
@@ -9,7 +9,8 @@ VALUES
     (37.7749, -122.4194, 'San Francisco', 'City', 'United States', 600.6, 'Mediterranean', 52),
     (-2.3326, 34.8255, 'Serengeti Plains', 'Savanna', 'Tanzania', 30000, 'Tropical', 1162),
     (-3.4653, -62.2159, 'Amazon Rainforest', 'Rainforest', 'Brazil', 706.7, 'Tropical', 200),
-	(23.4162, 15.5377, 'Sahara Desert', 'Desert', 'Multiple', 9065000, 'Hot desert', 500);
+	(23.4162, 15.5377, 'Sahara Desert', 'Desert', 'Multiple', 9065000, 'Hot desert', 500),
+    (32.5, 103.5, 'Minshan Mountains', 'Mountain', 'China', 70000, 'subtropical highland climate', 1000);
     
 
 
@@ -24,7 +25,9 @@ VALUES
     ('Golden Gate Park', 'Urban Park', 'Endangered', 'High', 37.7749, -122.4194),
     ('Serengeti Plains', 'Savanna', 'Stable', 'Low', -2.3326, 34.8255),
 	('Amazon Rainforest', 'Tropical Rainforest', 'Declining', 'High', -3.4653, -62.2159),
-	('Sahara Desert', 'Desert', 'Stable', 'Low', 23.4162, 15.5377);
+	('Sahara Desert', 'Desert', 'Stable', 'Low', 23.4162, 15.5377),
+    ('Mount Siguniang', 'Forest', 'Stable', 'Moderate', 32.5, 103.5),
+    ('Mount Daxue', 'Forest', 'Stable', 'Moderate', 32.5, 103.5);
     
 
 -- Habitat Threats
@@ -51,7 +54,8 @@ VALUES
     ('Canis lupus', 'Gray wolf', 'Least concern', 'North America, Eurasia'),
     ('Phoenicopterus roseus', 'Greater flamingo', 'Least concern', 'Africa, southern Europe to south-west Asia'),
     ('Felis catus', 'Domestic cat', 'Least concern', 'Worldwide'),
-    ('Homo sapiens', 'Human', 'Not evaluated', 'Worldwide');
+    ('Homo sapiens', 'Human', 'Not evaluated', 'Worldwide'),
+    ('Panthera uncia', 'Snow leopard', 'Vulnerable', 'Central and South Asia');
 
 -- Species Threats
 INSERT INTO SThreats (ScientificName, Threat)
@@ -69,7 +73,10 @@ VALUES
     ('Phoenicopterus roseus', 'Habitat'),
     ('Felis catus', 'Predation'),
     ('Felis catus', 'Competition'),
-    ('Homo sapiens', 'Degradation');
+    ('Homo sapiens', 'Degradation'),
+    ('Panthera uncia', 'habitat loss'),
+    ('Panthera uncia', 'poaching'),
+    ('Panthera uncia', 'retaliatory killings');
 
 
 
@@ -80,8 +87,10 @@ VALUES
     (2, 5000, 'Stable', 0.0, 25.0, 'Golden Gate Park', 'Gorilla beringei'),
     (3, 250, 'Increasing', 0.1, 2.5, 'Hyde Park', 'Phoenicopterus roseus'),
     (4, 15000, 'Stable', 0.0, 5.0, 'Ueno Park', 'Ailuropoda melanoleuca'),
-    (5, 100, 'Decreasing', -0.1, 0.5, 'Jardin des Tuileries', 'Pongo pygmaeus');
-
+    (5, 1000, 'Increasing', 0.1, 1, 'Mount Siguniang', 'Ailuropoda melanoleuca'),
+    (6, 500, 'Increasing', 0.07, 0.5, 'Mount Daxue', 'Ailuropoda melanoleuca'),
+    (7, 2000, 'Decreasing', -0.05, 10.0, 'Mount Siguniang', 'Panthera uncia');
+    
 -- Researcher
 INSERT INTO Researcher (Name, Email, Phone, Expertise, SpeciesScientificName, PopulationID)
 VALUES 
@@ -117,3 +126,23 @@ VALUES
 INSERT INTO CompanyResearcher (Name, CompanyName, JobTitle, Email)
 VALUES 
     ('Jennifer Johnson', 'Acme Inc.', 'Senior Biologist', 'johndoe@example.com');
+
+
+-- ConservationOrganization
+INSERT INTO ConservationOrganization (ContactEmail, Name, Mission, Website)
+VALUES
+    ('GPCO124@mail.com', 'Giant Panda Conservation Organization', 'Protect the Endangered Giant Panda Population in China', 'www.GPCOChina.org' ),
+    ('GorillaConservationor@example.com', 'African Mountain Gorilla Conservation Organization', 'Protect and help the Mountain Gorilla Population in Africa', 'www.MountainGorillaAfrica.com'),
+    ('MMGPCO000@mail.com', 'Minshan Mountains Conservation Organization', 'Protect the Species that Live in Minshan Mountains', 'www.ProtectMinshanMountainsHabitants.org' );
+    
+-- Protects
+INSERT INTO Protects (ContactEmail, PopulationID)
+VALUES
+	('GPCO124@mail.com', 5),
+    ('GPCO124@mail.com', 6),
+    ('MMGPCO000@mail.com', 5),
+    ('MMGPCO000@mail.com', 7),
+    ('GorillaConservationor@example.com',2);
+    
+    
+    
